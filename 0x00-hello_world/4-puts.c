da2b7d06fc429c9af95d8010d9aa5aa4f383f4ae
Ahmed Mokhtar <102578386+Ahmedmokhar@users.noreply.github.com>
2022-09-08 12:34:36 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-ls -l
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: prints "\"Programming is like building a multilingual puzzle"
+* Return: 0
+*/
+
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
