56e01ac40e8aec7a19f5f1b636d7a9dea58baf88
Ahmed Mokhtar <102578386+Ahmedmokhar@users.noreply.github.com>
2022-09-13 13:17:49 +0200

    Update 3-main_a.c

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include "holberton.h"
+#include <main.h>
 
 /**
- * main - takes a date and prints how many days are left in the year, taking
- * leap years into account
- * Return: 0
+ * main - takes a date and prints how many days are left in the year, taking leap years into account
  */
 
 int main(void)
