b2988ed3b13e46c2286f1b0676c97f9a81690a29
Ahmed Mokhtar <102578386+Ahmedmokhar@users.noreply.github.com>
2022-09-13 13:35:16 +0200

    Update 3-convert_day.c

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,8 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /*
- * convert_day - converts day of month to day of year, without accounting
- * for leap year
+ * Converts day of month to day of year, without accounting for leap year
  * @month: month in number format
  * @day: day of month
  * Return: day of year
