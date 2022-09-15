2f8f3f35f2cf6df47c22fdee8a925f17a755dff2
Ahmed Mokhtar <102578386+Ahmedmokhar@users.noreply.github.com>
2022-09-15 10:35:51 +0200

    Add files via upload

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - simple FIZZBUZZ program
+ *
+ * Return: Void
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
